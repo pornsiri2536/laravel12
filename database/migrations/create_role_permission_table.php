69750ba5d9a5809b92563a0a9507797aeec37280
pornsiri2536 <pornsiri.vei@vru.ac.th>
2026-04-19 12:54:51 +0700

    +

--- database/migrations/create_role_permission_table.php
+++
@@ -1,6 +0,0 @@
-Schema::create('role_permission', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
-    $table->foreignId('permission_id')->constrained('permissions')->onDelete('cascade');
-    $table->timestamps();
-});
--- database/migrations/create_user_role_table.php
+++
@@ -1,6 +0,0 @@
-Schema::create('user_role', function (Blueprint $table) {
-    $table->id();
-    $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-    $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
-    $table->timestamps();
-});
